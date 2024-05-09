dd2db7ca1fe108bfe38646e0960dd45c122cf7a1
Ayamba Sumaila <sumailaayamba15@gmail.com>
2024-05-09 22:55:46 +0000

    "Dynamic data added"

--- app.py
+++ app.py
@@ -6,7 +6,29 @@ app=Flask(__name__)
 @app.route("/")
 
 def home():
-    return render_template("home.html")
+
+    JOBS = [
+        {
+            'id':1,
+            'title':'Data Analyst',
+            'location':'Bengaluru, India',
+            'salary':'Rs. 10,00,000'
+        },
+        {
+            'id':2,
+            'title':'Data Scientist',
+            'location':'Delhi, India',
+            'salary':'Rs. 15,00,000'
+
+        },
+        {
+            'id':3,
+            'title':'Frontend Engineer',
+            'location':'Remote',
+            'salary':'Rs. 12,00,000'
+        }
+    ]
+    return render_template("home.html", jobs=JOBS)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
