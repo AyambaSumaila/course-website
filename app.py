8c6dcbde137983c7afffeeb35b34f8099e04d5a5
Ayamba Sumaila <sumailaayamba15@gmail.com>
2024-05-09 15:27:14 +0000

    "templates and static folders added"

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask #type:ignore
+from flask import Flask 
+from flask import render_template #type:ignore
 
 app=Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return "<h1>Sumaila, Hello World</h1>"
+    return render_template("home.html")
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
